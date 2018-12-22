9cdc9cd99d303acc13622568eb73b9100797e24c
Miguel C <MiguelC@iMac.local>
2018-12-22 15:23:02 +0100

    Changed image-upload-config

--- server/services/image-upload.js
+++ server/services/image-upload.js
@@ -1,7 +1,7 @@
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const config = require('../config/dev');
+const config = require('../config');
 
 aws.config.update({
   secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
