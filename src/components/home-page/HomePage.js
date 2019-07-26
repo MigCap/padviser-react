deb04a879e544224312c61db74bcf10bdc89fbd9
Miguel <miguelcapellan@gmail.com>
2019-07-26 21:31:24 +0200

    Fixed keys and link main home screen pic.

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = ({ history }) => {
   return (
     <div className="masthead">
       <div className="ui-container">
-        <img
-          src={process.env.PUBLIC_URL + 'img/worldmapblue.png'}
-          alt="worldmap"
-        />
+        <Link to="/rentals">
+          <img
+            src={process.env.PUBLIC_URL + 'img/worldmapblue.png'}
+            alt="worldmap"
+          />
+        </Link>
         <h2>
           Book or Rent WorldWide any AV equipement just with a few clicks.
         </h2>
