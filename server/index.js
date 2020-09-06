a6713712027948206ea71bff690bbc64e243052f
MigCap <miguelangel.capellan@vass.es>
2020-09-06 22:25:35 +0200

    most linting corrections

--- server/index.js
+++ server/index.js
@@ -1,56 +1,51 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const config = require('./config');
-const Rental = require('./models/rental');
-const FakeDb = require('./fake-db');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config");
+const Rental = require("./models/rental");
+const FakeDb = require("./fake-db");
+const path = require("path");
 
-const rentalRoutes = require('./routes/rentals'),
-  userRoutes = require('./routes/users'),
-  bookingRoutes = require('./routes/bookings'),
-  paymentRoutes = require('./routes/payments'),
-  imageUploadRoutes = require('./routes/image-upload'),
-  reviewRoutes = require('./routes/reviews');
+const rentalRoutes = require("./routes/rentals"),
+  userRoutes = require("./routes/users"),
+  bookingRoutes = require("./routes/bookings"),
+  paymentRoutes = require("./routes/payments"),
+  imageUploadRoutes = require("./routes/image-upload"),
+  reviewRoutes = require("./routes/reviews");
 
-mongoose
-  .connect(
-    config.DB_URI,
-    { useNewUrlParser: true }
-  )
-  .then(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      const fakeDb = new FakeDb();
-      // fakeDb.seedDb();
-    }
-  });
+mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(() => {
+  // if (process.env.NODE_ENV !== "production") {
+  //   const fakeDb = new FakeDb();
+  //   fakeDb.seedDb();
+  // }
+});
 
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/v1/rentals', rentalRoutes);
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/bookings', bookingRoutes);
-app.use('/api/v1/payments', paymentRoutes);
-app.use('/api/v1/reviews', reviewRoutes);
-app.use('/api/v1', imageUploadRoutes);
+app.use("/api/v1/rentals", rentalRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/bookings", bookingRoutes);
+app.use("/api/v1/payments", paymentRoutes);
+app.use("/api/v1/reviews", reviewRoutes);
+app.use("/api/v1", imageUploadRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  const appPath = path.join(__dirname, '..', 'build');
+if (process.env.NODE_ENV === "production") {
+  const appPath = path.join(__dirname, "..", "build");
   app.use(express.static(appPath));
 
-  app.get('*', function(req, res) {
-    res.sendFile(path.resolve(appPath, 'index.html'));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(appPath, "index.html"));
   });
 }
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, function() {
-  console.log('App is running!');
+app.listen(PORT, function () {
+  console.log("App is running!");
 });
--- server/models/user.js
+++ server/models/user.js
@@ -1,49 +1,49 @@
-const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: {
     type: String,
-    required: 'Username is require',
-    min: [4, 'Username too short. Need to be at least 4 characters'],
-    max: [12, 'Username too long. 12 characters max']
+    required: "Username is require",
+    min: [4, "Username too short. Need to be at least 4 characters"],
+    max: [12, "Username too long. 12 characters max"],
   },
   password: {
     type: String,
-    required: 'Password is require',
-    min: [8, 'Password must have at least 8 characters'],
-    max: [15, 'Password is too long, max is 15 characters']
+    required: "Password is require",
+    min: [8, "Password must have at least 8 characters"],
+    max: [15, "Password is too long, max is 15 characters"],
   },
   email: {
     type: String,
-    required: 'Email is require',
+    required: "Email is require",
     unique: true,
     lowercase: true,
-    min: [4, 'Username too short. Need to be at least 4 characters'],
-    max: [32, 'Too long. Max 32 characters'],
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+    min: [4, "Username too short. Need to be at least 4 characters"],
+    max: [32, "Too long. Max 32 characters"],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
   },
-  image: { type: String, required: true },
+  image: { type: String, required: false },
   stripeCustomerId: String,
   revenue: Number,
-  rentals: [{ type: Schema.Types.ObjectId, ref: 'Rental' }],
-  bookings: [{ type: Schema.Types.ObjectId, ref: 'Booking' }]
+  rentals: [{ type: Schema.Types.ObjectId, ref: "Rental" }],
+  bookings: [{ type: Schema.Types.ObjectId, ref: "Booking" }],
 });
 
-userSchema.methods.hasSamePassword = function(requestedPassword) {
+userSchema.methods.hasSamePassword = function (requestedPassword) {
   return bcrypt.compareSync(requestedPassword, this.password);
 };
 
-userSchema.pre('save', function(next) {
+userSchema.pre("save", function (next) {
   const user = this;
 
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(user.password, salt, function(err, hash) {
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(user.password, salt, function (err, hash) {
       user.password = hash;
       next();
     });
   });
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
